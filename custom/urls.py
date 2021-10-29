8597bbe7ed066b50e02367a98f0062deb37d251d
mahmut can kurt <is.mahmutcankurt@gmail.com>
2021-10-30 01:58:22 +0300

    html'ler ve urls oluşturuldu

--- custom/urls.py
+++ custom/urls.py
@@ -2,5 +2,19 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+
+
+    # articles
+
+    path('admin-articles', CustomAdminPageArticleView.as_view(), name="admin-articles"),
+
+    # categories
+
+    path('admin-categories', CustomAdminPageCategoryView.as_view(), name="admin-categories"),
+
+    # comments
+
+    path('admin-comments', CustomAdminPageCommentView.as_view(), name="admin-comments"),
     
+
 ]
